d633f93c8f977f584ac7eb68c90907c2b5cdf8d8
salah ubuntu <cloud.data.sa@gmail.com>
2024-09-10 17:07:51 +0300

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,24 @@
 import { title } from "@/components/primitives";
+import { siteConfig } from "@/config/site";
 
 export default function AboutPage() {
+  const a =
+    ""
+
+  const b = "."
+  const c = ""
   return (
-    <div>
-      <h1 className={title()}>About</h1>
+    <div className='text-start'>
+      <h1 className={title({class:"",size: "xs"})}>
+        تطبيق ماي سند
+      </h1>
+      <div className="h-5"></div>
+      <ol className="space-y-2 list-disc">
+
+      {siteConfig.aboutData.items.map((a,i)=> (
+        <li key={i} >{a}</li>
+      ))}
+      </ol>
     </div>
   );
 }
